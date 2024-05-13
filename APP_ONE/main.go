b328c98d09e73ef533ea7a517b5f9d9085ce4cb1
umesh <umeshsavaliya007@gmail.com>
2024-05-13 15:21:24 +0530

    make prgess in app

--- APP_ONE/main.go
+++ APP_ONE/main.go
@@ -1,9 +1,24 @@
+package main
+
 import (
-	"html/template"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func main(){
-	http.("/static")
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		temp := template.Must(template.ParseFiles("./templates/index.html"))
+		temp.Execute(w, nil)
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		temp := template.Must(template.ParseFiles("./templates/fregments"))
+		temp.Execute(w, nil)
+	})
+
+	log.Println("App runing on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
